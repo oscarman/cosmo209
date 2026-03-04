6a9db471bf7e70c95001896952dc12b5ce983633
Oscar Jhair Mantilla <oscarmantilla16@gmail.com>
2026-03-04 05:57:28 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js
+self.addEventListener('install', function(e){
+  console.log("Service Worker instalado");
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(fetch(e.request));
+});
