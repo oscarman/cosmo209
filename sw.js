413aef88716604d81fdbbc00d62bc7a06cdd99d2
Oscar Jhair Mantilla <oscarmantilla16@gmail.com>
2026-03-04 23:00:12 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-// sw.js
-self.addEventListener('install', function(e){
-  console.log("Service Worker instalado");
+const CACHE_NAME = "cosmos29-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', function(e){
-  e.respondWith(fetch(e.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
